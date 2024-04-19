5b3a2f8f11ffae88e18828c13a20e7682f421fe6
Jack Manzer <jack@ecatholic.com>
2024-04-19 13:13:26 -0500

    GEN-197 - increase unit test coverage for DB package

--- database/api_test.go
+++ database/api_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/beaconsoftwarellc/gadget/v2/database/qb"
 	"github.com/beaconsoftwarellc/gadget/v2/database/record"
 	"github.com/beaconsoftwarellc/gadget/v2/database/transaction"
+	"github.com/beaconsoftwarellc/gadget/v2/errors"
 	"github.com/beaconsoftwarellc/gadget/v2/generator"
 	assert1 "github.com/stretchr/testify/assert"
 	gomock "go.uber.org/mock/gomock"
@@ -82,6 +83,8 @@ func (t *metaTestRecord) Alias(alias string) *metaTestRecord {
 
 var MetaTestRecord = (&metaTestRecord{}).Alias("test_record")
 
+type TestRecordCollection []*TestRecord
+
 type countMatcher struct {
 	count int32
 }
@@ -119,6 +122,185 @@ func (matcher *queryMatcher) Matches(x interface{}) bool {
 
 func (matcher *queryMatcher) String() string {
 	return "*qb.SelectQuery"
+}
+
+func Test_SelectWithTotal(t *testing.T) {
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	api := NewMockAPI(ctrl)
+
+	var (
+		err    error
+		total  int
+		target TestRecordCollection
+		limit = 0
+		offset = 0
+	)
+
+	query := qb.Select(MetaTestRecord.ID).From(MetaTestRecord)
+
+	//limit equal zero 
+	api.EXPECT().Count(MetaTestRecord, query).Return(int32(2), nil)
+	
+	_, total, err = SelectWithTotal(api, MetaTestRecord, target, query, limit, offset)
+
+	assert.NoError(err)
+	assert.Equal(2, total)
+
+	//total equal zero 
+	limit = 1 //change limit
+
+	api.EXPECT().Count(MetaTestRecord, query).Return(int32(0), nil)
+	
+	_, total, err = SelectWithTotal(api, MetaTestRecord, target, query, limit, offset)
+
+	assert.NoError(err)
+	assert.Equal(0, total)
+
+	//total equal 1 
+	api.EXPECT().Count(MetaTestRecord, query).Return(int32(1), nil)
+	api.EXPECT().Select(&target, query, record.NewListOptions(limit, offset)).Return(nil)
+
+	_, total, err = SelectWithTotal(api, MetaTestRecord, target, query, limit, offset)
+	
+	assert.NoError(err)
+	assert.Equal(1, total)
+
+	//error cases
+	expected := generator.String(20)
+
+	api.EXPECT().Count(MetaTestRecord, query).Return(int32(0), errors.New(expected)) 
+
+	_, total, err = SelectWithTotal(api, MetaTestRecord, target, query, limit, offset)
+	assert.Equal(0, total)
+	assert.EqualError(err, expected)
+
+	api.EXPECT().Count(MetaTestRecord, query).Return(int32(1), nil)
+	api.EXPECT().Select(&target, query, record.NewListOptions(limit, offset)).Return(errors.New(expected))
+
+	_, total, err = SelectWithTotal(api, MetaTestRecord, target, query, limit, offset)
+	assert.EqualError(err, expected)
+	assert.Equal(0, total)
+}
+
+func Test_Begin(t *testing.T){
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+	var err error 
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+	err = api.Begin()
+	assert.NoError(err)
+}
+
+
+func Test_Rollback(t *testing.T){
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+	var err error 
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+
+	transaction.EXPECT().Rollback().Return(nil)
+
+	err = api.Rollback()
+
+	assert.NoError(err)
+}
+
+func Test_Rollback_transactionIsNil(t *testing.T){
+	assert := assert1.New(t)
+
+	api := &api{
+		tx:            nil,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+
+	err := api.Rollback()
+	assert.EqualError(err, ErrMissingTransaction.Error())
+}
+
+func Test_Commit(t *testing.T){
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+
+	var err error 
+
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+
+	transaction.EXPECT().Commit().Return(nil)
+
+	err = api.Commit()
+
+	assert.NoError(err)
+}
+
+func Test_Commit_transactionIsNil(t *testing.T){
+	assert := assert1.New(t)
+
+	api := &api{
+		tx:            nil,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+
+	err := api.Commit()
+	assert.EqualError(err, ErrMissingTransaction.Error())
+}
+
+func Test_ApiMethods(t *testing.T) {
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+
+	target := &TestRecord{ID: "2", Name: "test"}
+	listOptions := &record.ListOptions{Limit: 1, Offset: 0}
+	query := qb.Select(MetaTestRecord.ID).From(MetaTestRecord)
+	conditionalExpression := qb.FieldComparison(MetaTestRecord.Name, qb.Equal, "")
+
+	transaction.EXPECT().Create(target)
+	api.Create(target)
+
+	transaction.EXPECT().Read(target, target.PrimaryKey()).Return(nil)
+	api.Read(target, target.PrimaryKey())
+	
+	transaction.EXPECT().ReadOneWhere(target, conditionalExpression).Return(nil)
+	api.ReadOneWhere(target, conditionalExpression)
+
+	transaction.EXPECT().Select(target, query, *listOptions).Return(nil)
+	api.Select(target, query, listOptions)
+
+	transaction.EXPECT().ListWhere(target, target, conditionalExpression, *listOptions).Return(nil)
+	api.ListWhere(target, target, conditionalExpression, listOptions)
+
+	transaction.EXPECT().Update(target).Return(nil)
+	api.Update(target)
+
+	transaction.EXPECT().UpdateWhere(target, conditionalExpression, qb.FieldValue{Field: MetaTestRecord.ID, Value: 2 }).Return(int64(0), nil)
+	total, err := api.UpdateWhere(target, conditionalExpression, qb.FieldValue{Field: MetaTestRecord.ID, Value: 2 })
+
+	assert.Equal(int64(0), total)
+	assert.NoError(err)
+
+	transaction.EXPECT().Delete(target).Return(nil)
+	api.Delete(target)
+
+	transaction.EXPECT().DeleteWhere(target, conditionalExpression).Return(nil)
+	api.DeleteWhere(target, conditionalExpression)
 }
 
 func Test_database_enforceLimits(t *testing.T) {
@@ -177,6 +359,44 @@ func Test_api_Count(t *testing.T) {
 	actual, err := api.Count(MetaTestRecord, query)
 	assert.NoError(err)
 	assert.Equal(expected, actual)
+}
+
+func Test_api_Count_zero(t *testing.T) {
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+	query := qb.Select(MetaTestRecord.ID).From(MetaTestRecord)
+	transaction.EXPECT().Select(gomock.Any(),
+		&queryMatcher{t: t, sql: "SELECT COUNT(*) as count FROM " +
+			"`test_record` AS `test_record`"},
+		record.ListOptions{Limit: 1, Offset: 0},
+	).Return(nil)
+	actual, err := api.Count(MetaTestRecord, query)
+	assert.NoError(err)
+	assert.Equal(int32(0), actual)
+}
+
+func Test_api_Count_error(t *testing.T) {
+	assert := assert1.New(t)
+	ctrl := gomock.NewController(t)
+	transaction := transaction.NewMockTransaction(ctrl)
+	api := &api{
+		tx:            transaction,
+		configuration: &InstanceConfig{MaxLimit: 100},
+	}
+	expected := generator.String(20)
+	query := qb.Select(MetaTestRecord.ID).From(MetaTestRecord)
+	transaction.EXPECT().Select(gomock.Any(),
+		&queryMatcher{t: t, sql: "SELECT COUNT(*) as count FROM " +
+			"`test_record` AS `test_record`"},
+		record.ListOptions{Limit: 1, Offset: 0},
+	).Return(errors.New(expected))
+	_, err := api.Count(MetaTestRecord, query)
+	assert.EqualError(err, expected)
 }
 
 func Test_api_CountWhere_nil(t *testing.T) {
