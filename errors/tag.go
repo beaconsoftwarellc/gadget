6ce4ee880a6aaa958006a1643bb351d6ada39e62
Anthony Pynes <anthony.pynes@gmail.com>
2024-10-15 19:42:13 -0400

    [COR-1185] preserve original error

--- errors/tag.go
+++ errors/tag.go
@@ -1,8 +1,6 @@
 package errors
 
 import (
-	"fmt"
-
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -15,25 +13,47 @@ type HasCode interface {
 	GetCode() codes.Code
 }
 
+type taggedError struct {
+	*status.Status
+	err error
+}
+
+func (e taggedError) Error() string {
+	return e.Status.Message()
+}
+
 // Tag the passed error
 func Tag(tag string, base HasCode) error {
-	return status.Error(base.GetCode(),
-		fmt.Sprintf("[%s]: %s", tag, base.Error()))
+	if base == nil {
+		return nil
+	}
+	return TagAndCode(tag, base.GetCode(), base)
 }
 
 // TagAndCode the passed error
 func TagAndCode(tag string, code codes.Code, base error) error {
-	return status.Error(code, fmt.Sprintf("[%s]: %s", tag, base.Error()))
-}
-
-// GetOriginalError returns the original error from the passed status error
-func GetOriginalError(statusError error) error {
-	if statusError == nil {
+	if base == nil {
 		return nil
 	}
-	_status, ok := status.FromError(statusError)
-	if !ok {
+	return &taggedError{
+		Status: status.Newf(code,
+			"[%s]: %s", tag, base.Error()),
+		err: base,
+	}
+}
+
+// GetBase returns the original error from the passed tagged error
+func GetBase(err error) error {
+	if err == nil {
+		return nil
+	}
+	if tagged, ok := err.(*taggedError); ok {
+		return tagged.err
+	}
+	// status does not preserve the original error, but
+	// better than nothing
+	if _status, ok := status.FromError(err); !ok {
 		return _status.Err()
 	}
-	return statusError
+	return err
 }
