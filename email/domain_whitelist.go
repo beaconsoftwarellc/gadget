0386a6d3cd4f049c164bebaae90f256b18b6045c
emi <voxelost@proton.me>
2025-07-15 13:43:54 +0200

    add docs

--- email/domain_whitelist.go
+++ email/domain_whitelist.go
@@ -10,6 +10,8 @@ type whitelist []string
 //go:embed resources/whitelist.txt
 var rawDomains string
 
+// NewWhitelist creates a new domain whitelist from an embedded list of allowed domains.
+// Returns a whitelist that can be used to validate if email domains are allowed.
 func NewWhitelist() whitelist {
 	var wl []string
 	for _, domainLine := range strings.Split(rawDomains, "\n") {
@@ -23,6 +25,9 @@ func NewWhitelist() whitelist {
 	return whitelist(wl)
 }
 
+// Validate checks if the provided domain is present in the whitelist.
+// It performs an exact string match against all domains in the whitelist.
+// Returns true if the domain is found in the whitelist, false otherwise.
 func (w whitelist) Validate(domain string) bool {
 	for _, validDomain := range w {
 		if domain == validDomain {
