9a6a5974a6ea69637b396a7c43ee8e28a59a5f04
Olaf Szymański <olaf.szym123@gmail.com>
2025-10-15 16:58:47 +0200

    [GEN-221] Add CountDistinctExpression

--- database/qb/count.go
+++ database/qb/count.go
@@ -5,7 +5,8 @@ import (
 )
 
 const (
-	countSQL = "COUNT(*) as count"
+	countSQL         = "COUNT(*) as count"
+	countDistinctSQL = "COUNT(DISTINCT %s) as count"
 )
 
 // RowCount serves as a target for queries returning only a count of rows
@@ -39,4 +40,32 @@ func (ce *CountExpression) GetTables() []string {
 // ParameterizedSQL that represents this count expression
 func (ce *CountExpression) ParameterizedSQL() (string, []any) {
 	return countSQL, nil
+}
+
+// CountDistinctExpression can be used as the expression in count distinct query's. Requires
+// a [TableField] that exists in the query to bind to for validation.
+type CountDistinctExpression struct {
+	tableField TableField
+}
+
+// NewCountDistinctExpression of the passed [TableField] for use in a SelectExpression
+func NewCountDistinctExpression(tableField TableField) SelectExpression {
+	return &CountDistinctExpression{
+		tableField: tableField,
+	}
+}
+
+// GetName of this count distinct expression
+func (cde CountDistinctExpression) GetName() string {
+	return fmt.Sprintf("COUNT(DISTINCT %s)", cde.tableField.GetName())
+}
+
+// GetTables used by this count distinct expression
+func (cde CountDistinctExpression) GetTables() []string {
+	return cde.tableField.GetTables()
+}
+
+// ParameterizedSQL that represents this count distinct expression
+func (cde CountDistinctExpression) ParameterizedSQL() (string, []any) {
+	return fmt.Sprintf(countDistinctSQL, cde.tableField.SQL()), nil
 }
--- database/qb/count_test.go
+++ database/qb/count_test.go
@@ -23,3 +23,23 @@ func Test_NewCountExpression(t *testing.T) {
 	assert.Equal(expectedSQL, actualSQL)
 	assert.Nil(values)
 }
+
+func Test_NewCountDistinctExpression(t *testing.T) {
+	assert := assert1.New(t)
+	expectedTable := generator.String(20)
+	expectedField := generator.String(20)
+	expression := NewCountDistinctExpression(TableField{
+		Name:  expectedField,
+		Table: expectedTable,
+	})
+	assert.Equal(1, len(expression.GetTables()))
+	assert.Equal(expectedTable, expression.GetTables()[0])
+
+	expectedName := fmt.Sprintf("COUNT(DISTINCT %s)", expectedField)
+	assert.Equal(expectedName, expression.GetName())
+
+	expectedSQL := fmt.Sprintf("COUNT(DISTINCT `%s`.`%s`) as count", expectedTable, expectedField)
+	actualSQL, values := expression.ParameterizedSQL()
+	assert.Equal(expectedSQL, actualSQL)
+	assert.Nil(values)
+}
