36e1c75b2c2ddeb5f2af639bfb1a25e5cb61d45b
Anthony Pynes <anthony.pynes@gmail.com>
2025-12-02 18:03:34 -0500

    [COR-1444] updating interface for event

--- eventstream/eventstream.go
+++ eventstream/eventstream.go
@@ -1,10 +1,17 @@
 package eventstream
 
 type Event interface {
-	GetProject() string  // this should be static and with Resource identifies the stream
-	GetResource() string // this should be static and with Action identifies the stream
-	GetAction() string
-	GetPayload() []byte
+	// GetPartitionKey used to determine which shard this Event belongs
+	// to. This key is md5'd and then bucketed into the hash space of the
+	// number of shards. Any events who sequencing is important should be
+	// on the same shard and should use the same partition key. e.g. all
+	// events for actions on a resource should have the resource id as the
+	// partition key.
+	//
+	// PartitionsKey does NOT have to be unique per [Event].
+	GetPartitionKey() string
+	// GetPayload as an immutable blob of byte data.
+	GetPayload() ([]byte, error)
 }
 
 type EventStream[T Event] interface {
@@ -12,4 +19,8 @@ type EventStream[T Event] interface {
 	Get() <-chan T
 	// Put an event record into the underlying Kinesis Stream
 	Put(T) error
+}
+
+func New[T Event](project, resource string) EventStream[T] {
+	return nil
 }
